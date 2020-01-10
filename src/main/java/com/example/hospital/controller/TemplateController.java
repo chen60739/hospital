7fcd2236576d185048937a90558b614ea148ba0d
chen60739 <892760739@qq.com>
2020-01-10 17:38:19 +0800

    陈：找回列表

--- src/main/java/com/example/hospital/controller/TemplateController.java
+++ src/main/java/com/example/hospital/controller/TemplateController.java
@@ -1,8 +1,13 @@
 package com.example.hospital.controller;
 
+import com.example.hospital.dto.TemplateOverView;
+import com.example.hospital.model.Lable;
 import com.example.hospital.model.Template;
 import com.example.hospital.model.TemplateSet;
+import com.example.hospital.service.impl.LableServiceImpl;
 import com.example.hospital.service.impl.TemplateServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,16 +26,32 @@ import java.util.Map;
 @Controller
 public class TemplateController {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(TemplateController.class);
+
     @Autowired
     private TemplateServiceImpl templateService;
+    @Autowired
+    private LableServiceImpl lableService;
 
+    /**
+     * 保存模板
+     * @param tempName 模板名称
+     * @param lableId 表单控件id
+     * @return
+     */
     @RequestMapping("/saveTemp")
     @ResponseBody
     public Map<String, String> createTemp(@RequestParam("tempName") String tempName,
                                           @RequestParam("lableId") String lableId){
-        templateService.saveTemplate(tempName,lableId);
-        Map<String,String> res = new HashMap<>();
-        res.put("mes","message");
+        Map<String, String> res = templateService.saveTemplate(tempName, lableId);
+        return res;
+    }
+
+    @RequestMapping("updateTemp")
+    @ResponseBody
+    public Map<String, String> updateTemplate(@RequestParam("tempId") Integer tempId,
+                                              @RequestParam("lableId") String lableId){
+        Map<String, String> res = templateService.updateTemp(tempId,lableId);
         return res;
     }
 
@@ -44,9 +65,17 @@ public class TemplateController {
 
     @RequestMapping("/tempListDate")
     @ResponseBody
-    public List<Template> tempListData(@RequestParam("departmentId") Integer departmentId){
-        List<Template> list = templateService.getTemplateList(departmentId);
+    public List<Template> tempListData(@RequestParam("groupId") Integer groupId){
+        List<Template> list = templateService.getTemplateList(groupId);
         return list;
+    }
+
+    @RequestMapping("/getTemp")
+    @ResponseBody
+    public Map<String, List> getTemp(@RequestParam("groupId")Integer groupId,
+                                     @RequestParam("departmentId") Integer departmentId){
+        Map<String, List> res = templateService.getTemp(groupId, departmentId);
+        return res;
     }
 
     @RequestMapping("/design")
@@ -54,10 +83,18 @@ public class TemplateController {
                         @RequestParam("departmentId") Integer departmentId,
                          Model model){
         List<TemplateSet> list = templateService.getLablesByTempId(tempId);
+        Template t = templateService.selectOneTemplate(tempId);
         model.addAttribute("lables",list);
-        model.addAttribute("tempId",tempId);
+        model.addAttribute("temp",t);
         model.addAttribute("departmentId",departmentId);
         return "template_design";
+    }
+
+    @RequestMapping("/getLables")
+    @ResponseBody
+    public List<Lable> getLables(@RequestParam("tempId") Integer tempId){
+        List<Lable> lables = lableService.getLablesByTempId(tempId);
+        return lables;
     }
 
     @RequestMapping("/publishTemp")
@@ -67,10 +104,42 @@ public class TemplateController {
         return res;
     }
 
-    @RequestMapping("/delTemplate")
+    @RequestMapping("/removeTemplate")
     @ResponseBody
-    public Map<String, String> delTemplate(@RequestParam("tempId") Integer tempId){
-        Map<String, String> res = templateService.delTemplate(tempId);
+    public Map<String, String> delTemplate(@RequestParam("tempId") Integer tempId,
+                                           @RequestParam("groupId") Integer groupId){
+        Map<String, String> res = templateService.removeTemplate(groupId,tempId);
         return res;
+    }
+
+    /**
+     * 保存选择的模板
+     * @param groupId
+     * @param tempIds
+     * @return
+     */
+    @RequestMapping("/saveGroupSelectTemp")
+    @ResponseBody
+    public Map<String,String> saveGroupSelectTemp(@RequestParam("groupId")Integer groupId,
+                                                  @RequestParam("tempIds")String tempIds){
+        Map<String, String> res = null;
+        try {
+            res = templateService.setGroupSelectTemp(groupId,tempIds);
+            LOGGER.info("TemplateController-----saveGroupSelectTemp  成功" +"groupId:" +groupId +"tempIds:"+ tempIds);
+        }catch (Exception e){
+            LOGGER.info("TemplateController-----saveGroupSelectTemp  失败"+ e +"groupId:" +groupId +"tempIds:"+ tempIds);
+        }
+        return res;
+    }
+
+    @RequestMapping("/templateOverviewData")
+    @ResponseBody
+    public List<TemplateOverView> templateOverviewData(@RequestParam("groupName")String groupName,
+                                                       @RequestParam("startTime")String startTime,
+                                                       @RequestParam("endTime")String endTime,
+                                                       @RequestParam("tempName")String tempName,
+                                                       @RequestParam("createPeople")String createPeople){
+        List<TemplateOverView> list = templateService.templateOverviewData(groupName,startTime,endTime,tempName,createPeople);
+        return list;
     }
 }
