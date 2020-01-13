f87b353ce6f115c682b2bf8b85bdc55db4408bb2
user <571895781@qq.com>
2020-01-13 15:52:12 +0800

    王斌  新增用户权限

--- src/main/java/com/example/hospital/service/GroupUserRoleService.java
+++ src/main/java/com/example/hospital/service/GroupUserRoleService.java
@@ -3,6 +3,7 @@ package com.example.hospital.service;
 import com.example.hospital.dto.SupGroupUserRole;
 import com.example.hospital.dto.SuperUser;
 import com.example.hospital.model.Dictionary;
+import com.example.hospital.model.GroupUserRole;
 import com.example.hospital.model.User;
 import org.apache.ibatis.annotations.Param;
 
@@ -15,21 +16,56 @@ import java.util.List;
  */
 public interface GroupUserRoleService {
 
-    //权限管理页面--查询
+    /**
+     * 权限管理页面--查询
+     * @param occupationId
+     * @param groupId
+     * @return
+     */
     List<SupGroupUserRole> findAllGroupUser(String occupationId,Integer groupId);
 
-    //批量删除
+    /**
+     * 批量删除
+     * @param ids
+     * @return
+     */
     int removeById(String ids);
 
-    //根据科室id查询 姓名
+    /**
+     * 根据科室id查询 姓名
+     * @param departmentId
+     * @param userName
+     * @return
+     */
     List<SupGroupUserRole> findNameByDepartmentId(Integer departmentId ,String userName);
 
-    //新增用户
+    /**
+     * 新增用户
+     * @param groupUserId
+     * @param id
+     * @param groupRoleId
+     * @return
+     */
     HashMap<String,Object> changeGroupUserRole(String groupUserId, Integer id, String groupRoleId);
 
-    //根据id查询权限名字
+    /**
+     * 根据id查询权限名字
+     * @param id
+     * @return
+     */
     Dictionary findGroupRoleName(String id);
-    //根据id查询用户
+
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
     User findNameById(String id);
+    /**
+     * 新增用户权限
+     * @param groupUserRole
+     * @return
+     */
+    int saveGroupUserRole(GroupUserRole groupUserRole);
 
 }
